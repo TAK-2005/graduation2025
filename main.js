5a31e1f280bfb4c5af2d1276d3d7fc8ee8145ba6
TAK-2005 <toako.yama05@gmail.com>
2025-12-12 10:12:32 +0900

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // **Instructions** **main.js**
 // ------------
-// In this document you will find instructions on how to adjust different parameters of the paradigm. You can apply the desired changes to the document main.js on your computer or server, using a source code editor.
-// The following parameters are necessary to adjust: number of avatar images, and the redirect link at the end of the study. All other parameters have a default option and adjustments are optional.
+// 修正版: 条件を2つ（排斥・包含）に限定し、クアルトリクス連携機能を追加
 
 $(function() {
 
@@ -11,57 +10,27 @@ $(function() {
   function set_settings() {
     window.settings = [];
 	
-	// **Number** **of** **Avatar** **Images**   
-	// Number of avatars the user can choose from. Can be changed to any number, depending on how many avatars you would like to display. Default: 82
-	// The avatar images used in the online preview of the paradigm were created using by pickaface.net and due to their terms not available for redistribution. You should therefore create your own images. All images should be 250x250 pixels in size and carry the names "avatar_NUMBER.png" (e.g. avatar_1.png; "png" should be lower case; the numbers in the names should be consequtive, starting from 1). The number of avatars dependeds on the corresponding parameter. The images should be placed in folder "avatars," located in the main study folder extracted on your computer or server.
-
     settings.numberofavatars = 24;
 
-	
-    // **Redirection**    
-	// After the introduction task is over participants should be redirected to a survey with manipulation checks and dependent measures, to subsequent tasks, or to further instructions. 
-	// If the study is called with a parameter for redirection, as explained in the documentation, this value is overwritten. 
-	// To the redirect link, the following information will be appended: (1) participant number, (2) condition, (3) username, (4) description submitted by participant. These variables can be extracted from the link, saved as data, and used for linking the Social Media Ostracism paradigm to subsequent tasks and measures. See documentation for more details.
-
     settings.defaultredirect = 'https://osakashakaishinri.qualtrics.com/jfe/preview/previewId/f9dfef6f-813b-434e-a7fc-56452861ef4a/SV_50GU6ZV97rbZEX4?Q_CHL=preview&Q_SurveyVersionID=current';
-
 	
-	// **Tasklength**     
-    // Length of the group introduction task in milliseconds. Can be changed to any number (in ms). Default: 180000 (3min) 
     settings.tasklength = 180000; 
 
-	
-	// **Number** **of** **"likes"**    
-    // Each received "like" is indicated by the timepoint (in ms) at which the "like" will appear. To change the number of "likes" in each condition, add or remove timepoints. Make sure that every timepoint (except the first) is preceded by a single comma. 
-	// In cases with only 1 "like," a second "like" is added with time point 9999999. This "like" is added for programming purposes and is never executed, as it is outside the task time
-
-    // In condition 1, the participant will receive 1 like at the following timepoint (in ms). Default: [12000, 9999999]
+    // *** 修正: 条件を2つに変更 ***
+    // condition 1 = 排斥 (exclusion): 1回のいいね
     settings.condition_1_likes = [12000, 9999999]; 
 
-    // In condition 2, user will receive 6 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_2_likes = [10000, 15000,35000,80000,1320000,150000];  
-    
-    // In condition 3, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
-    settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
+    // condition 2 = 包含 (inclusion): 6回のいいね
+    settings.condition_2_likes = [10000, 15000, 35000, 80000, 132000, 150000];  
 
-	// **Others' likes**     
-	// To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
-	settings.condition_1_adjusted_likes = [12000, 14000,15000,35000,80000,100000,110000,150000,20000]; // 9
-	settings.condition_2_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
-	settings.condition_3_adjusted_likes = [12000, 9999999]; //1	
+    // 他の参加者が受け取るいいね数（全体のバランスを保つため）
+	settings.condition_1_adjusted_likes = [12000, 14000, 15000, 35000, 80000, 100000, 110000, 150000, 20000]; // 9
+	settings.condition_2_adjusted_likes = [12000, 14000, 15000, 35000, 80000]; // 5
 	
-    // Usernames by which the participant will receive "likes"
-	// If group member names are changed, these should be changed accordingly.
     settings.likes_by = ['みなみ','Kim','彩','ハル','Y.K','陽菜','蓮','MK','ユウト']; 
   }
   
-  // -------------------
-  // Above were the basic parameters you can adjust using the instructions. The remaining code is also annotated, but we do not recommend changing it, unless you are comfortable with web programming.
-  // -------------------
   
-  
-  // **Slide:** **Intro**     
-  // With instructions regarding the task. The intro container is shown, the continue calls the next slide when clicked.
   function init_intro() {
   	$('#intro').show();
   	$('#submit_intro').on('click',function() {
@@ -70,14 +39,10 @@ $(function() {
   	});	
   }
   
-
-  // **Slide:** **Username**       
-  // Note: Only alphanumeric usernames without spaces are accepted
   
   function init_name() {
 
   	$('#name').show();
-
     
   	$('#submit_username').on('click',function() {
 
@@ -91,8 +56,6 @@ $(function() {
   		}
 
 　　　   function isValidName(name){ 
-        // アルファベット・ひらがな・カタカナ・漢字を許可
-        // スペースや数字、記号は不可
         return /^[A-Za-z\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF]+$/.test(name);
       }
   		if(!isValidName(uname)) {
@@ -108,12 +71,9 @@ $(function() {
   			alertify.log(errormsg,"error");
   		}
 
-
   	});
   }
 
-  // **Slide:** **Avatar**       
-  // Avatar slide in which the participant is asked to select an avatar
    
   function init_avatar() {
   	$('#avatar').show();
@@ -143,7 +103,6 @@ $(function() {
   }
 
 
-  // **Slide:** **Description**   
   function init_text() {
   	$('#text').show();
 
@@ -179,7 +138,6 @@ $(function() {
   }
 
 
-  // **Slide:** **Instructions**   
   function init_fb_intro() {
   	$('#fb_intro').show();
 	
@@ -192,8 +150,6 @@ $(function() {
   }
 
 
-  // **Slide:** **Login** **Screen**   
-  // Participant can continue after 8000ms = 8s      
   function init_fb_login() {
   	$('#fb_login').show();
 	
@@ -209,7 +165,7 @@ $(function() {
   	});	
   }
   
-  // **Slide:** **Task**   
+  
   function init_task() {
 
     $('#task').show();
@@ -217,7 +173,7 @@ $(function() {
 	shortcut.add("Backspace",function() {});      
 
   	jQuery("#countdown").countDown({
-  		startNumber: window.settings.tasklength/1000, // in seconds
+  		startNumber: window.settings.tasklength/1000,
   		callBack: function(me) {
   			console.log('over');
         $('#timer').text('00:00');
@@ -236,15 +192,12 @@ $(function() {
 		  ]
 		};
 		
-    // Add user box to slide     
 	  var tpl = $('#usertmp').html(),html = Mustache.to_html(tpl, users);
 	  $("#task").append(html);
 	  
-    // Add other boxes to slide    
 	  var tpl = $('#otherstmp').html(),html = Mustache.to_html(tpl, others);
 	  $("#task").append(html);
  
-    // Randomize order of other players boxes
     function reorder() {
        var grp = $("#others").children();
        var cnt = grp.length;
@@ -261,7 +214,6 @@ $(function() {
     }
     reorder();    
 
-    // When user receives likes
 	  $('.userslikes').each(function() {
   		var that = $(this);
   		var usernames = $(this).data('usernames').split(",");
@@ -281,7 +233,6 @@ $(function() {
   		} 		
 	  });
 	  
-    // When others receive likes
 	  $('.otherslikes').each(function() {
   		var that = $(this);
   		var times = $(this).data('likes').split(",");
@@ -298,22 +249,18 @@ $(function() {
 	  });
 	 
 
-    // Initialize like buttons
 	  $('.btn-like').on('click', function() {
 		  $(this).prev().text(parseInt($(this).prev().text()) + 1);
-      // Like buttons can only be clicked once
 		  $(this).attr("disabled", true);
 	  });
 
-    // Initalize Masonry plugin
-    // For display of user and other players boxes in columns without gaps
 		$('#task').masonry({
 		  itemSelector : '.entry',
 		  columnWidth : 10
 		});
 
 
-    // Redirect, default after 180000ms = 180s = 3min
+    // *** 修正: クアルトリクスへの完了通知機能を追加 ***
     setTimeout(function() {
     
     $(window).unbind('beforeunload');
@@ -324,34 +271,36 @@ $(function() {
     
     $('#final-continue').on('click', function() {
 
-      // Redirect link
-      location.href = window.redirect+'&p='+window.participant+'&c='+window.condition+'&u='+encodeURI(window.username)+'&av='+window.avatarexport+'&d='+encodeURI(window.description);
+      // クアルトリクスに完了を通知
+      if (window.parent && window.parent.Qualtrics) {
+        window.parent.Qualtrics.SurveyEngine.setEmbeddedData('experimentCompleted', 'true');
+      }
+      
+      // postMessageで親ウィンドウに通知
+      window.parent.postMessage({type: 'ostracismComplete'}, '*');
 
     });
     
-    },window.settings.tasklength); // timing for task
+    },window.settings.tasklength);
 
   }
 	
 
-  // Get URL parameters to set condition number and participant number
 function get_params() {
 
-// condition number must be 1, 2, or 3
-    if(window.QueryString.c !== undefined && !isNaN(parseInt(window.QueryString.c)) && parseInt(window.QueryString.c) > 0 && parseInt(window.QueryString.c) < 4) {
+// *** 修正: 条件を2つに限定（1 = 排斥, 2 = 包含）***
+    if(window.QueryString.c !== undefined && !isNaN(parseInt(window.QueryString.c)) && parseInt(window.QueryString.c) > 0 && parseInt(window.QueryString.c) < 3) {
       window.condition = parseInt(window.QueryString.c);
     } else {
-      window.condition = 1; // condition defaults to 1
+      window.condition = 1;
     }
 
-    // participant number must be numeric
     if(window.QueryString.p !== undefined && !isNaN(parseInt(window.QueryString.p))) {
       window.participant = parseInt(window.QueryString.p);
     } else {
-      window.participant = 0; // participant defaults to 0
+      window.participant = 0;
     }    
 
-    // redirect
     if(window.QueryString.redirect !== undefined && window.QueryString.redirect !== "") {
       window.redirect = decode(window.QueryString.redirect);
     } else {
@@ -362,16 +311,13 @@ function get_params() {
     if(!urlHasQuestionMark) {
         window.redirect = window.redirect + "?redir=1";
     }
-	//alert(window.redirect);
 }
 
   
   
-  // adjustments according to current condition
   function adjust_to_condition() {
 
-    // the number of likes a person receives depends on the condition
-	// in addition, the number of likes another person receives is adjusted, so that there is the same number of likes overall
+    // *** 修正: 条件を2つに変更 ***
 	switch(condition) {
 		case 1:
 			window.settings.condition_likes = settings.condition_1_likes;
@@ -381,31 +327,22 @@ function get_params() {
 			window.settings.condition_likes = settings.condition_2_likes;
 			window.others.posts[1].likes = settings.condition_2_adjusted_likes;
 			break;
-		case 3:
-			window.settings.condition_likes = settings.condition_3_likes;
-			window.others.posts[1].likes = settings.condition_3_adjusted_likes;
-			break;
 	}	
 	  
   }
   
 
-  // The variable QueryString contains the url parameters, i.e. condition no. and participant no.
-  // via http://stackoverflow.com/a/979995
   window.QueryString = function () {
     var query_string = {};
     var query = window.location.search.substring(1);
     var vars = query.split("&");
     for (var i=0;i<vars.length;i++) {
       var pair = vars[i].split("=");
-        // If first entry with this name
       if (typeof query_string[pair[0]] === "undefined") {
         query_string[pair[0]] = pair[1];
-        // If second entry with this name
       } else if (typeof query_string[pair[0]] === "string") {
         var arr = [ query_string[pair[0]], pair[1] ];
         query_string[pair[0]] = arr;
-        // If third or later entry with this name
       } else {
         query_string[pair[0]].push(pair[1]);
       }
@@ -414,8 +351,6 @@ function get_params() {
   } ();
 
 
-  // Function to check letters and numbers
-  // via http://www.w3resource.com/javascript/form/letters-numbers-field.php
   function not_alphanumeric(inputtxt) {
     var letterNumber = /^[0-9a-zA-Z]+$/;
     if(inputtxt.match(letterNumber)) {
@@ -426,14 +361,10 @@ function get_params() {
   }
 
 
-  // Function to add extra zeros infront of numbers (used for the countdown)
-  // via http://stackoverflow.com/a/6466243
   function pad (str, max) {
     return str.length < max ? pad("0" + str, max) : str;
   }
 
-  // Function for encoding and decoding URLs
-  // via http://meyerweb.com/eric/tools/dencoder/
   function encode(unencoded) {
 	return encodeURIComponent(unencoded).replace(/'/g,"%27").replace(/"/g,"%22");	
   }
@@ -442,8 +373,6 @@ function get_params() {
   }
 
   
-  // Simple Countdown
-  // via http://davidwalsh.name/jquery-countdown-plugin
   jQuery.fn.countDown = function(settings,to) {
     settings = jQuery.extend({
       startFontSize: "12px",
@@ -473,18 +402,15 @@ function get_params() {
     });
   };
 
-  // Prevent that participants accidentally exit the experiment by disabling F5 and backspace keys
   shortcut.add("f5",function() {});  
   $(window).bind('beforeunload', function(){
     return 'Are you sure you want to quit the experiment completely?';
   });   
 
-  // Set Settings, get Participant No. and Condition No.
   set_settings();
   get_params();
   adjust_to_condition();
 
-  // Start with the intro slide
   init_intro();
 
 });
